b42f26d82fcd0f6ec86166e34a07c5519bb93daf
kayesoriano <99286709+kayesoriano@users.noreply.github.com>
2022-02-08 12:45:16 -0800

    Add files via upload

---
+++ R_Introduction.R
@@ -0,0 +1,15 @@
+library(Biostrings)
+library(seqinr)
+# Load CRAN packages
+library(tidyverse)
+library(vegan)
+# Load Bioconductor packages
+library(phyloseq)
+library(DESeq2)
+library(rlang)
+# Load libraries for class
+library(rmarkdown)
+library(nycflights13)
+
+#import files
+?readxl
